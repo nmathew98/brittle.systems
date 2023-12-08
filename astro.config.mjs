2520179ab103983a4199b9e7cab5bcd72346fdfb
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-12-09 12:01:23 +1300

    chore: add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://brittle.systems",
   integrations: [tailwind(), mdx({
     optimize: true
-  })],
+  }), sitemap()],
   prefetch: true,
   markdown: {
     shikiConfig: {
