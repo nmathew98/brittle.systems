fddd035a64867ea516e440317e14ac5b8f3822a4
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-12-09 11:57:01 +1300

    chore: add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import cloudflare from "@astrojs/cloudflare";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), mdx({
+    optimize: true
+  })],
   markdown: {
     shikiConfig: {
       theme: 'github-dark',
