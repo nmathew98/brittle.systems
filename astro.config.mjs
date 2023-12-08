809eedbbb69660133fe3676fe1a9140248858095
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-12-09 12:04:18 +1300

    chore: pnpm format

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,29 +2,32 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import cloudflare from "@astrojs/cloudflare";
 import mdx from "@astrojs/mdx";
-
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://brittle.systems",
-  integrations: [tailwind(), mdx({
-    optimize: true
-  }), sitemap()],
-  prefetch: true,
-  markdown: {
-    shikiConfig: {
-      theme: 'github-dark',
-      wrap: true
-    }
-  },
-  output: "server",
-  adapter: cloudflare({
-    mode: "directory",
-    imageService: "cloudflare",
-    runtime: {
-      type: "pages",
-      mode: "off"
-    }
-  })
+	site: "https://brittle.systems",
+	integrations: [
+		tailwind(),
+		mdx({
+			optimize: true,
+		}),
+		sitemap(),
+	],
+	prefetch: true,
+	markdown: {
+		shikiConfig: {
+			theme: "github-dark",
+			wrap: true,
+		},
+	},
+	output: "server",
+	adapter: cloudflare({
+		mode: "directory",
+		imageService: "cloudflare",
+		runtime: {
+			type: "pages",
+			mode: "off",
+		},
+	}),
 });
