a7e8737de74917808e1a25c00a2d2dea1260bc4a
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-12-09 11:54:34 +1300

    chore: add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,14 @@ export default defineConfig({
       theme: 'github-dark',
       wrap: true
     }
-  }
+  },
+  output: "server",
+  adapter: cloudflare({
+    mode: "directory",
+    imageService: "cloudflare",
+    runtime: {
+      type: "pages",
+      mode: "off"
+    }
+  })
 });
