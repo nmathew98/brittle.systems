99d9cefabfb09d4265e9572a41910d254abad457
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-12-10 18:11:43 +1300

    fix: focus issues and make combobox uncontrolled

--- src/components/Landing/Search.tsx
+++ src/components/Landing/Search.tsx
@@ -13,8 +13,7 @@ export interface Article {
 }
 
 export const Search = ({ articles = [] as Article[] }) => {
-	const inputRef = React.useRef<HTMLInputElement | null>(null);
-	const [selected, setSelected] = React.useState<Article | null>(null);
+	const comboboxButtonRef = React.useRef<HTMLButtonElement | null>(null);
 	const [query, setQuery] = React.useState("");
 
 	const recentlyUpdatedCategories = [
@@ -41,25 +40,20 @@ export const Search = ({ articles = [] as Article[] }) => {
 						includes(article.tags, query),
 			  );
 
-	React.useEffect(() => {
-		if (selected) {
-			location.pathname = selected.url;
-		}
-	}, [selected]);
-
-	// TODO: When clicking on a category, the options show but directly clicking on option
-	// causes it to just dismiss, you have to interact with the input for clicking on an
-	// option to work
 	return (
 		<div>
-			<Combobox value={selected} onChange={setSelected} nullable>
+			<Combobox
+				defaultValue={null}
+				onChange={value => {
+					location.pathname = value.url;
+				}}
+				nullable>
 				{({ open }) => (
 					<>
 						<div className="relative mt-1">
 							<div className="relative w-full cursor-default overflow-hidden bg-white text-left shadow-lg focus:outline-none sm:text-sm">
 								<Combobox.Input
 									autoFocus
-									ref={inputRef}
 									className="w-full border-none py-4 pl-3 pr-10 text-lg bg-sky-950 transition focus-visible:bg-transparent focus:outline-none text-white placeholder-slate-100 focus-visible:placeholder-slate-800 focus-visible:text-slate-800 font-semibold"
 									displayValue={article =>
 										(article as Article)?.title
@@ -75,7 +69,9 @@ export const Search = ({ articles = [] as Article[] }) => {
 										setQuery(event.target.value)
 									}
 								/>
-								<Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+								<Combobox.Button
+									ref={comboboxButtonRef}
+									className="absolute inset-y-0 right-0 flex items-center pr-2">
 									<ChevronUpDownIcon
 										className="h-5 w-5 text-slate-400"
 										aria-hidden="true"
@@ -89,44 +85,41 @@ export const Search = ({ articles = [] as Article[] }) => {
 								leaveTo="opacity-0"
 								show={Boolean(query) || open}
 								afterLeave={() => setQuery("")}>
-								{(query || open) && (
-									<Combobox.Options
-										static
-										className="absolute mt-1 max-h-60 w-full overflow-auto rounded-xs bg-white py-1 focus:outline-none text-base sm:text-sm">
-										{filtered.length === 0 &&
-										query !== "" ? (
-											<div className="relative cursor-default select-none px-4 py-2 text-slate-800 font-medium">
-												No articles found
-											</div>
-										) : (
-											filtered.map(person => (
-												<Combobox.Option
-													key={person.url}
-													className={({ active }) =>
-														`relative select-none px-4 py-2 ${
-															active
-																? "bg-sky-950 text-white"
-																: "text-slate-800"
-														}`
-													}
-													value={person}>
-													{({ selected }) => (
-														<>
-															<span
-																className={`block truncate ${
-																	selected
-																		? "font-semibold"
-																		: "font-normal"
-																}`}>
-																{person.title}
-															</span>
-														</>
-													)}
-												</Combobox.Option>
-											))
-										)}
-									</Combobox.Options>
-								)}
+								<Combobox.Options
+									static
+									className="absolute mt-1 max-h-60 w-full overflow-auto rounded-xs bg-white py-1 focus:outline-none text-base sm:text-sm">
+									{filtered.length === 0 && query !== "" ? (
+										<div className="relative cursor-default select-none px-4 py-2 text-slate-800 font-medium">
+											No articles found
+										</div>
+									) : (
+										filtered.map(person => (
+											<Combobox.Option
+												key={person.url}
+												className={({ active }) =>
+													`relative select-none px-4 py-2 ${
+														active
+															? "bg-sky-950 text-white"
+															: "text-slate-800"
+													}`
+												}
+												value={person}>
+												{({ selected }) => (
+													<>
+														<span
+															className={`block truncate ${
+																selected
+																	? "font-semibold"
+																	: "font-normal"
+															}`}>
+															{person.title}
+														</span>
+													</>
+												)}
+											</Combobox.Option>
+										))
+									)}
+								</Combobox.Options>
 							</Transition>
 							{/* Absolute because otherwise when options open there is a layout shift */}
 							{!(query || open) && (
@@ -141,8 +134,7 @@ export const Search = ({ articles = [] as Article[] }) => {
 													key={category}
 													onClick={() => {
 														setQuery(category);
-														inputRef.current?.click();
-														inputRef.current?.focus();
+														comboboxButtonRef.current?.click();
 													}}
 													className="text-white font-medium focus:italic focus:outline-none">
 													{category}
