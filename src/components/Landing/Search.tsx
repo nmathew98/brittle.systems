542d065739eb96c163e8f9953bcb10b01af031b4
Naveen Mathew <55116576+nmathew98@users.noreply.github.com>
2023-12-11 20:19:09 +1300

    fix: adding article last modified date to options

--- src/components/Landing/Search.tsx
+++ src/components/Landing/Search.tsx
@@ -90,7 +90,7 @@ export const Search = ({ articles = [] as Article[] }) => {
 										<Combobox.Option
 											key={article.url}
 											className={({ active }) =>
-												`relative select-none px-4 py-5 ${
+												`relative select-none px-4 py-4 ${
 													active
 														? "bg-orange-600 text-white"
 														: "text-slate-800"
@@ -109,6 +109,10 @@ export const Search = ({ articles = [] as Article[] }) => {
 																: "font-normal"
 														}`}>
 														{article.title}
+														<br />
+														<span className="text-sm sm:text-xs">
+															{article.modified.toDateString()}
+														</span>
 													</span>
 												</>
 											)}
