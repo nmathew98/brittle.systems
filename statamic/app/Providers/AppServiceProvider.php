0342fac01324481ef88acde759d8c5537138f001
Tyrone Biggums <55116576+nmathew98@users.noreply.github.com>
2024-11-02 20:37:43 +1300

    fix: force https in production and development (#103)

--- statamic/app/Providers/AppServiceProvider.php
+++ statamic/app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 use Statamic\Statamic;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production' || env('APP_ENV') === 'development') {
+            URL::forceScheme('https');
+        }
+
         // Statamic::vite('app', [
         //     'resources/js/cp.js',
         //     'resources/css/cp.css',
